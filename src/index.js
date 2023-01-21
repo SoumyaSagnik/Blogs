70063cef69fdbf5c6b9c22204163fd2b2c32a665
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-01-21 23:24:24 +0530

    Scroll top on page transition added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
