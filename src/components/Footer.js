e4f8bc1293ca0469450de7baa51b038dd52bb3c3
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-01-23 05:20:49 +0530

    css update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,25 +3,44 @@ const Footer = ({ screenSize }) => {
     <footer style={{ top: screenSize }}>
       <ul>
         <li>
-          Author:{" "}
-          <p>
-            <a
-              href="http://portfolio.soumyakhanda.tech/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Soumya Sagnik Khanda
-            </a>
-          </p>
+          <a
+            href="http://portfolio.soumyakhanda.tech/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={require(`../images/name.png`)} alt="SSK" id="name" />
+          </a>
         </li>
         <li>
-          Phone: <p>8093438354</p>
+          <a
+            href="https://github.com/SoumyaSagnik/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../images/github.png`)}
+              alt="GitHub"
+              id="github"
+            />
+          </a>
         </li>
         <li>
-          Email:{" "}
-          <p>
-            <a href="mailto:soumyasagnik@gmail.com">soumyasagnik@gmail.com</a>
-          </p>
+          <a
+            href="https://www.linkedin.com/in/soumya-sagnik-khanda-0446641b8/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              src={require(`../images/linkedin.png`)}
+              alt="linkedin"
+              id="linkedin"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="mailto:soumyasagnik@gmail.com">
+            <img src={require(`../images/email.png`)} alt="mail" id="mail" />
+          </a>
         </li>
       </ul>
     </footer>
